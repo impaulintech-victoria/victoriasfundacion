15de45ca2151010937cacfc3ccdbddfcdab08adc
raldev08 <gerald@victoriasorigen.email>
2025-06-09 17:57:23 +0800

    feat(hero): add background image for hero section

--- src/components/pages/home/achievement/achievementLandingContainer.tsx
+++ src/components/pages/home/achievement/achievementLandingContainer.tsx
@@ -1,50 +1,50 @@
-import FadedBgImage from '@/components/common/FadedBgImage'
-import AchievementCard from './achievementCard'
+import FadedBgImage from "@/components/common/FadedBgImage";
+import AchievementCard from "./achievementCard";
 
 const AchievementsLandingContainer = () => {
   return (
-    <div className='relative px-5 py-8 sm:py-10 lg:py-12 2xl:py-18 xl:py-14 w-full'>
+    <div className="relative px-5 py-8 sm:py-10 lg:py-12 2xl:py-18 xl:py-14 w-full">
       <FadedBgImage
-        src={'/landing/achievements/landing-achievement-bg.png'}
-        alt='achievement-bg'
-        bgColor='[#F5E1E6]'
+        src={"/landing/achievements/landing-achievement-bg.png"}
+        alt="achievement-bg"
+        bgColor="[#F5E1E6]"
       />
-      <div className='z-10 relative flex flex-col justify-center items-center gap-7 2xl:gap-14 h-full'>
-        <h2 className='bg-accent px-4 py-1 border border-primary rounded-full text-base 2xl:text-lg'>
+      <div className="z-10 relative flex flex-col justify-center items-center gap-7 2xl:gap-14 h-full">
+        <h2 className="bg-accent px-4 py-1 border border-primary rounded-full text-base 2xl:text-lg">
           Wat we bereiken
         </h2>
-        <section className='flex flex-col items-center gap-3'>
-          <h1 className='font-bold text-primary text-4xl 2xl:text-5xl'>
+        <section className="flex flex-col items-center gap-3">
+          <h1 className="font-bold text-primary text-4xl 2xl:text-5xl">
             Onze Impact
           </h1>
-          <p className='max-w-xs 2xl:max-w-2xl text-muted text-sm 2xl:text-lg text-center'>
+          <p className="max-w-xs 2xl:max-w-2xl text-muted text-sm 2xl:text-lg text-center">
             Samen maken we het verschil voor gezinnen die onze steun het hardst
             nodig hebben.
           </p>
         </section>
         <section
-          id='cards-section'
-          className='flex sm:flex-row flex-col items-stretch gap-3 lg:gap-5 2xl:gap-10 xl:gap-7 h-full'
+          id="cards-section"
+          className="flex sm:flex-row flex-col items-stretch gap-3 lg:gap-5 2xl:gap-10 xl:gap-7 h-full"
         >
           <AchievementCard
-            imageSrc={'/landing/achievements/achievement-card-1.png'}
-            title='100+'
-            description='Gezinnen ondersteund met directe hulp en begeleiding'
+            imageSrc={"/landing/achievements/achievement-card-1.png"}
+            title="100+"
+            description="Gezinnen ondersteund met directe hulp en begeleiding"
           />
           <AchievementCard
-            imageSrc={'/landing/achievements/achievement-card-2.png'}
-            title='5'
-            description='Lopende projecten gericht op duurzame verandering'
+            imageSrc={"/landing/achievements/achievement-card-2.png"}
+            title="5"
+            description="Lopende projecten gericht op duurzame verandering"
           />
           <AchievementCard
-            imageSrc={'/landing/achievements/achievement-card-3.png'}
-            title='€50K+'
+            imageSrc={"/landing/achievements/achievement-card-3.png"}
+            title="€50K+"
             description="Verzameld ten behoeve van ondersteuningsprogramma's"
           />
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AchievementsLandingContainer
+export default AchievementsLandingContainer;
--- src/components/pages/home/callToAction/callToActionLandingContainer.tsx
+++ src/components/pages/home/callToAction/callToActionLandingContainer.tsx
@@ -1,16 +1,16 @@
-import HeroCenterTextButton from '@/components/common/Hero/HeroCenterTextButton'
+import HeroCenterTextButton from "@/components/common/Hero/HeroCenterTextButton";
 
 const CallToActionLandingContainer = () => {
   return (
     <HeroCenterTextButton
-      tag='Make a Difference for Families'
-      header='Help ons om meer gezinnen te ondersteunen'
-      description='Elke bijdrage, groot of klein, maakt een verschil in het leven van gezinnen die onze hulp nodig hebben. Samen kunnen we voor meer mensen een hoopvolle toekomst creëren.'
-      buttonLabel='Word donateur'
-      imageSrc='/landing/cta-image.png'
+      tag="Make a Difference for Families"
+      header="Help ons om meer gezinnen te ondersteunen"
+      description="Elke bijdrage, groot of klein, maakt een verschil in het leven van gezinnen die onze hulp nodig hebben. Samen kunnen we voor meer mensen een hoopvolle toekomst creëren."
+      buttonLabel="Word donateur"
+      imageSrc="/landing/cta-image.png"
       imageOpacity={0.95}
     />
-  )
-}
+  );
+};
 
-export default CallToActionLandingContainer
+export default CallToActionLandingContainer;
