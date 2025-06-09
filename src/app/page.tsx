4f3c64ec7836814748bd31a8ebc208f6941d57dc
raldev08 <gerald@victoriasorigen.email>
2025-06-09 17:57:23 +0800

    feat(hero): add background image for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return <div className='text-h1'>Hello World</div>
+  return (
+    <div id='main-container' className='flex w-full flex-col'>
+      <section
+        id='hero-background'
+        className='relative flex w-full justify-end z-0 bg-[#521D2A]'
+      >
+        <div className='relative'>
+          <Image
+            src='/landing/landing-image.png'
+            alt='Hero Background'
+            width={1920}
+            height={1080}
+            className='object-cover w-[1000px] h-[700px] z-0'
+          />
+          <div className='absolute top-0 left-0 w-full h-full z-20 pointer-events-none bg-gradient-to-r from-[#521D2A] via-[#521D2A]/70 to-transparent' />
+        </div>
+      </section>
+    </div>
+  )
 }
