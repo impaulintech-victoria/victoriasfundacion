fc1476d8df3dd16046aa4b3579bc474530da7296
raldev08 <gerald@victoriasorigen.email>
2025-06-10 20:01:24 +0800

    rebased to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import HeroLandingContainer from '@/components/pages/home/Hero/HeroLandingContainer'
 import AchievementsLandingContainer from '@/components/pages/home/achievement/achievementLandingContainer'
 import CallToActionLandingContainer from '@/components/pages/home/callToAction/callToActionLandingContainer'
@@ -6,10 +7,19 @@ import MissionLandingContainer from '@/components/pages/home/mission/missionLand
 export default function Home() {
   return (
     <div id='main-container' className='flex flex-col w-full'>
+=======
+import HeroLandingContainer from "@/components/pages/home/hero/HeroLandingContainer";
+import AchievementsLandingContainer from "@/components/pages/home/achievement/achievementLandingContainer";
+import MissionLandingContainer from "@/components/pages/home/mission/missionLandingContainer";
+
+export default function Home() {
+  return (
+    <div id="main-container" className="flex flex-col gap-10 w-full">
+>>>>>>> 22934ab (rebased to main)
       <HeroLandingContainer />
       <MissionLandingContainer />
       <AchievementsLandingContainer />
       <CallToActionLandingContainer />
     </div>
-  )
+  );
 }
