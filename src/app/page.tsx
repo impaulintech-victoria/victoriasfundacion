faf8ed5aa011b0354e0525ccbf6815b5c6b327e9
raldev08 <gerald@victoriasorigen.email>
2025-06-10 22:01:13 +0800

    feat(cta): implement call to action section with reusable bg image and card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,22 @@ export default function Home() {
 =======
 =======
 import AchievementsLandingContainer from "@/components/pages/home/achievement/achievementLandingContainer";
+<<<<<<< HEAD
 >>>>>>> 62348b1 (feat(achievements): create achievements section with reusable card)
+=======
+import CallToActionLandingContainer from "@/components/pages/home/callToAction/callToActionLandingContainer";
+>>>>>>> a691aa9 (feat(cta): implement call to action section with reusable bg image and card)
 import HeroLandingContainer from "@/components/pages/home/hero/HeroLandingContainer";
-import AchievementsLandingContainer from "@/components/pages/home/achievement/achievementLandingContainer";
 import MissionLandingContainer from "@/components/pages/home/mission/missionLandingContainer";
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <div id="main-container" className="flex flex-col gap-10 w-full">
 >>>>>>> 22934ab (rebased to main)
+=======
+    <div id="main-container" className="flex flex-col w-full">
+>>>>>>> a691aa9 (feat(cta): implement call to action section with reusable bg image and card)
       <HeroLandingContainer />
       <MissionLandingContainer />
       <AchievementsLandingContainer />
