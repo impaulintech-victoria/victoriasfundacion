f5626bd7a1ba7b50a9b8a08e67e9808d105e2abe
raldev08 <gerald@victoriasorigen.email>
2025-06-11 20:31:17 +0800

    fix: resolve ci errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import HeroLandingContainer from '@/components/pages/home/Hero/HeroLandingContainer'
 import AchievementsLandingContainer from '@/components/pages/home/achievement/achievementLandingContainer'
 import CallToActionLandingContainer from '@/components/pages/home/callToAction/callToActionLandingContainer'
+import HeroLandingContainer from '@/components/pages/home/hero/HeroLandingContainer'
 import MissionLandingContainer from '@/components/pages/home/mission/missionLandingContainer'
 
 export default function Home() {
   return (
-    <div id='main-container' className='flex flex-col w-full'>
-=======
-=======
-import AchievementsLandingContainer from "@/components/pages/home/achievement/achievementLandingContainer";
-<<<<<<< HEAD
->>>>>>> 62348b1 (feat(achievements): create achievements section with reusable card)
-=======
-import CallToActionLandingContainer from "@/components/pages/home/callToAction/callToActionLandingContainer";
->>>>>>> a691aa9 (feat(cta): implement call to action section with reusable bg image and card)
-import HeroLandingContainer from "@/components/pages/home/hero/HeroLandingContainer";
-import MissionLandingContainer from "@/components/pages/home/mission/missionLandingContainer";
-
-export default function Home() {
-  return (
-<<<<<<< HEAD
-    <div id="main-container" className="flex flex-col gap-10 w-full">
->>>>>>> 22934ab (rebased to main)
-=======
-    <div id="main-container" className="flex flex-col w-full">
->>>>>>> a691aa9 (feat(cta): implement call to action section with reusable bg image and card)
+    <div id='main-container' className='flex flex-col gap-10 w-full'>
       <HeroLandingContainer />
       <MissionLandingContainer />
       <AchievementsLandingContainer />
       <CallToActionLandingContainer />
     </div>
-  );
+  )
 }
