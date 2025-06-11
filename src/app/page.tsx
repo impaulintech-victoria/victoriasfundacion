832f11c159a98dd3945e39f864dc362a8a7a571d
impaulintech-victoria <john@victoriasorigen.email>
2025-06-11 18:19:01 +0800

    fix: fix ci error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import AchievementsLandingContainer from "@/components/pages/home/achievement/achievementLandingContainer";
-import CallToActionLandingContainer from "@/components/pages/home/callToAction/callToActionLandingContainer";
-import HeroLandingContainer from "@/components/pages/home/hero/HeroLandingContainer";
-import MissionLandingContainer from "@/components/pages/home/mission/missionLandingContainer";
+import HeroLandingContainer from '@/components/pages/home/Hero/HeroLandingContainer'
+import AchievementsLandingContainer from '@/components/pages/home/achievement/achievementLandingContainer'
+import CallToActionLandingContainer from '@/components/pages/home/callToAction/callToActionLandingContainer'
+import MissionLandingContainer from '@/components/pages/home/mission/missionLandingContainer'
 
 export default function Home() {
   return (
-    <div id="main-container" className="flex flex-col w-full">
+    <div id='main-container' className='flex flex-col w-full'>
       <HeroLandingContainer />
       <MissionLandingContainer />
       <AchievementsLandingContainer />
       <CallToActionLandingContainer />
     </div>
-  );
+  )
 }
