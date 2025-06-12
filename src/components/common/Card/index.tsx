f5f6269fa01223d5c5f69603ee8ad8e8259949cb
raldev08 <gerald@victoriasorigen.email>
2025-06-12 20:49:51 +0800

    feat(support-family): implement support a family page

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,37 +1,37 @@
-'use client'
-import { ReactNode } from 'react'
+"use client";
+import { ReactNode } from "react";
 
 interface CardProps {
-  children: ReactNode
-  className?: string
-  padding?: string
-  rounded?: string
-  shadow?: boolean
-  isComingSoon?: boolean
+  children: ReactNode;
+  className?: string;
+  padding?: string;
+  rounded?: string;
+  shadow?: boolean;
+  isComingSoon?: boolean;
 }
 
 const Card = ({
   children,
-  className = '',
-  padding = 'p-6',
-  rounded = 'rounded-3xl',
+  className = "",
+  padding = "p-6",
+  rounded = "rounded-3xl",
   shadow = true,
   isComingSoon = false,
 }: CardProps) => {
   return (
     <div
       className={`relative bg-white ${rounded} ${padding} ${
-        shadow ? 'shadow-[0_10px_30px_rgba(0,0,0,0.05)]' : ''
+        shadow ? "shadow-[0_10px_30px_rgba(0,0,0,0.05)]" : ""
       } ${className}`}
     >
       {isComingSoon && (
-        <div className='z-10 absolute inset-0 flex justify-center items-center backdrop-blur-xs p-6 rounded-3xl'>
-          <span className='font-bold text-primary text-xl'>Coming Soon</span>
+        <div className="z-10 absolute inset-0 flex justify-center items-center backdrop-blur-xs p-6 rounded-3xl">
+          <span className="font-bold text-primary text-xl">Coming Soon</span>
         </div>
       )}
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
