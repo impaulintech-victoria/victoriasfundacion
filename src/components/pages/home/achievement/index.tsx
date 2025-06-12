5d8dd78d62deb07c036cae17e74f8991064cf233
impaulintech-victoria <john@victoriasorigen.email>
2025-06-12 13:10:48 +0800

    refactor(app): clean up achievements card component

--- src/components/pages/home/achievement/index.tsx
+++ src/components/pages/home/achievement/index.tsx
@@ -1,5 +1,5 @@
 import FadedBgImage from '@/components/common/FadedBgImage'
-import AchievementCard from './achievementCard'
+import Image from 'next/image'
 
 const AchievementsLandingContainer = () => {
   const achievements = [
@@ -45,12 +45,28 @@ const AchievementsLandingContainer = () => {
           className='flex sm:flex-row flex-col items-stretch gap-3 lg:gap-5 2xl:gap-10 xl:gap-7 h-full'
         >
           {achievements.map(({ imageSrc, title, description }, index) => (
-            <AchievementCard
+            <div
               key={index}
-              imageSrc={imageSrc}
-              title={title}
-              description={description}
-            />
+              id='achievement-card'
+              className='group flex flex-col flex-1 gap-4 bg-white shadow-lg px-3 lg:px-5 2xl:px-7 py-5 lg:py-7 2xl:py-10 border border-black rounded-xl'
+            >
+              <Image
+                src={imageSrc}
+                alt={title}
+                width={500}
+                height={500}
+                className='items-center group-hover:shadow-md group-hover:rounded-lg w-[250px] lg:w-[300px] 2xl:w-[400px] h-[150px] lg:h-[200px] 2xl:h-[300px] object-cover group-hover:scale-105 transition-all duration-300 ease-in-out'
+              />
+              <h1 className='font-bold text-primary text-xl lg:text-2xl 2xl:text-4xl text-center group-hover:scale-110 transition-all duration-300 ease-in-out'>
+                {title}
+              </h1>
+              <section className='flex flex-col items-center gap-2 lg:gap-4 2xl:gap-6'>
+                <span className='bg-accent border border-primary rounded-full w-[130px] lg:w-[150px] 2xl:w-[200px] group-hover:w-full h-[7px] transition-all duration-300 ease-in-out' />
+                <p className='max-w-[250px] lg:max-w-sm 2xl:max-w-sm text-muted text-sm lg:text-base 2xl:text-xl text-center'>
+                  {description}
+                </p>
+              </section>
+            </div>
           ))}
         </section>
       </div>
