8cb86b6ab5bb9de809985e89a5c53e946be6e148
raldev08 <gerald@victoriasorigen.email>
2025-06-13 16:05:05 +0800

    [VIC-036] - Implemented Contact page with React hook forms (#15)
    * feat(support-family): implement support a family page
    
    * feat(support-family): implement support a family page
    
    * feat(contact): implement contact page
    
    * fix(cta): resolved cta not showing image
    
    * fix(card): resolved card comments
    
    * feat(contact): implement contact page
    
    * [VIC-029] - Implemented Support a Family page (#14)
    
    * feat(support-family): implement support a family page
    
    * feat(support-family): implement support a family page
    
    * fix(cta): resolved cta not showing image
    
    * fix(card): resolved card comments
    
    * [VIC-043] - Setup Sentry and 404 page (#16)
    
    * feat(app): add custom 404 not-found page
    
    * feat(app): add custom 404 not-found page
    
    * chore: update logo from origen to fundacion
    
    * feat(support-family): implement support a family page
    
    * feat(contact): implement contact page
    
    * feat(contact): implement contact page
    
    * fix(build): fixed package json errors
    
    * fix(cta): cta section no image
    
    ---------
    
    Co-authored-by: John <john@victoriasorigen.email>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,18 @@
+import HeroSection from '@/components/common/Hero'
+import ContactFormSection from '@/components/pages/contact'
+
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className='relative flex flex-col w-full'>
+      <HeroSection
+        heading='Neem Contact Op'
+        description='Heeft u een vraag, wilt u samenwerken, of heeft u ondersteuning nodig? We horen graag van u.'
+        backgroundImg='/assets/images/contact/contact-bg-image.png'
+        height='sm'
+      />
+      <ContactFormSection />
+    </div>
+  )
 }
 
 export default Contact
