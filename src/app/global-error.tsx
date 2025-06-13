7ed6bb6bbeacab452a2bc289bdc575cbcc8a4278
impaulintech-victoria <john@victoriasorigen.email>
2025-06-13 12:59:39 +0800

    feat(app): add custom 404 not-found page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,18 +1,20 @@
 'use client'
 
 import * as Sentry from '@sentry/nextjs'
+import { getLocale } from 'next-intl/server'
 import NextError from 'next/error'
 import { useEffect } from 'react'
 
-export default function GlobalError({
+export default async function GlobalError({
   error,
 }: { error: Error & { digest?: string } }) {
   useEffect(() => {
     Sentry.captureException(error)
   }, [error])
+  const locale = await getLocale()
 
   return (
-    <html lang='nl'>
+    <html lang={locale}>
       <body>
         <NextError statusCode={0} />
       </body>
